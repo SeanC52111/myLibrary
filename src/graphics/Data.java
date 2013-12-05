787cde3e373a0e3d5be0dee35c67eedb28ec6615
topskychen <topskychen@gmail.com>
2013-12-05 17:20:02 +0800

    Add the drawing tools.

---
+++ src/graphics/Data.java
@@ -0,0 +1,93 @@
+package graphics;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+
+import com.sun.org.apache.xml.internal.security.Init;
+
+public class Data {
+	/**
+	 * 
+	 */
+	public static boolean isInit = false; 
+	public static double sx = Double.MAX_VALUE, sy = Double.MAX_VALUE, mx = Double.MIN_VALUE, my = Double.MIN_VALUE;
+	ArrayList<Point> points = new ArrayList<Point>();
+	Color color = null;
+	int drawType = 0;
+
+	public Data() {
+	}
+	
+	public Data(double[][] points, Color color) {
+		for (double[] point : points) {
+			Data.updateMargin(point[0], point[1]);
+		}
+		for (double[] point : points) {
+			addPoint(Data.parseFromLatLng(point[0], point[1]));
+		}
+		this.color = color;
+		isInit = true;
+		setPointType();
+	}
+	
+	public Data(Point[] points, Color color) {
+		for (int i = 0; i < points.length; i ++) {
+			this.points.add(points[i]);
+		}
+		this.color = color;
+		setPointType();
+	}
+	
+	public Data(ArrayList<Point> points, Color color) {
+		this.points = points;
+		this.color = color;
+		setPointType();
+	}
+	
+	public static void updateMargin(double lat, double lng) {
+		if (lat > mx) mx = lat;
+		if (lat < sx) sx = lat;
+		if (lng > my) my = lng;
+		if (lng < sy) sy = lng;
+		isInit = true;
+	}
+	
+	public boolean checkMargin() {
+		if (sx > mx || sy > my) return false;
+		return true;
+	}
+	
+	public void addPoint(Point p) {
+		points.add(p);
+	}
+	
+	public static Point parseFromLatLng(double lat, double lng) {
+		if (isInit == false) {
+			throw new IllegalStateException("The margins may be not initializedl.");
+		}
+		int x = (int)(((lat - sx) / (mx - sx)) * ShowData.DEFAULT_WIDTH);
+		int y = (int)(((lat - sx) / (mx - sx)) * ShowData.DEFAULT_WIDTH);
+		return new Point(x, y);
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	public boolean isPointType() {
+		return (drawType & 1) == 1 ? true : false;
+	} 
+	
+	public void setPointType() {
+		drawType |= 1;
+	} 
+	
+	public boolean isLineType() {
+		return ((drawType >> 1) & 1) == 1 ? true : false;
+	}
+	
+	public void setLineType() {
+		drawType |= (1 << 1);
+	}
+}
---
+++ src/graphics/ShowData.java
@@ -0,0 +1,111 @@
+package graphics;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import math.MathUtility;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author chenqian
+ *
+ *	This file is for showing data visually. 
+ *	Function loadMap() loads the map of nodes and roads.
+ *	Function loadData() is to load trajectory.
+ *	The format for the trajectory is (timestamp, vid, lat, lng)
+ *	Function loadRsu() is to load Rsus as the name shows.
+ *	The format of the data refers to the one in Converter.java.
+ *
+ */
+public class ShowData extends JFrame{
+
+	
+	public static final int DEFAULT_WIDTH = 800;
+	public static final int DEFAULT_HEIGHT = 800;
+	public static final int DEFAULT_L_X = 150;
+	public static final int DEFAULT_L_Y = 150;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Data data = new Data(new double[][]{
+				{39.991376, 116.32641}, 
+				{39.991358,116.326438},
+				{39.991364,116.326458},
+				}, Color.BLACK);
+		ShowData showData = new ShowData(new Data[]{data});
+		ShowData.draw(showData);
+	}
+	
+	public static void draw(final ShowData showData) {
+		try{
+			EventQueue.invokeAndWait(new Runnable() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					showData.setVisible(true);
+					showData.setResizable(true);
+					showData.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				}
+			});
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public ShowData(Data[] datas){
+		setSize(DEFAULT_WIDTH + 8, DEFAULT_HEIGHT + 30);
+		setLocation(DEFAULT_L_X, DEFAULT_L_Y);
+		add(new DataPanel(datas));
+	}
+	
+	class DataPanel extends JPanel{
+		ArrayList<Data> datas = new ArrayList<>();
+		DataPanel(Data[] datas){
+			for (Data data : datas) {
+				this.datas.add(data);
+			}
+		}
+		
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			for (Data data : datas) {
+				 g.setColor(data.color);
+				 if (data.isPointType()) {
+					 for (int j = 0; j < data.points.size(); j ++) {
+						 Point p = data.points.get(j);
+						g.drawRect(p.x, p.y, 5, 5); 
+					 }
+				 }
+				 if (data.isLineType()) {
+					 for (int j = 0; j < data.points.size() - 1; j ++) {
+						 Point p = data.points.get(j);
+						 Point q = data.points.get(j + 1);
+						 g.drawLine(p.x, p.y, q.x, q.y);
+					 }
+				 }
+			}
+			Toolkit.getDefaultToolkit().sync();
+			g.dispose();
+		}
+	}
+}
